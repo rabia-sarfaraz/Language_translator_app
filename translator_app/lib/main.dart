61dd477b5a7c15c3db001042f5866d13e03db53a
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2025-09-07 15:35:53 +0500

    changes

--- translator_app/lib/main.dart
+++ translator_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'splash_screen.dart';
-import 'home_screen.dart';
+import 'screens/splash_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const TranslatorApp());
