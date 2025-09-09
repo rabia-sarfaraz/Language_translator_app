b9d72cd63ec56be9f4fed9de5b7303d72cb400b0
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2025-09-09 12:15:41 +0500

    changes

--- translator_app/lib/screens/home_screen.dart
+++ translator_app/lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,59 +7,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Pure white background
+      backgroundColor: Colors.white, // White background
       body: Stack(
         children: [
-          /// Ellipse with exact size, position and rotation
+          /// Ellipse image (tum apna path dogi assets/images/ellipse.png)
           Positioned(
-            left: 0, // X position set karo agar adjust karna ho
-            top: -2.59, // Y position
-            child: Transform.rotate(
-              angle: -11.26 * pi / 180, // Rotation in radians
-              child: Container(
-                width: 508.36,
-                height: 588.24,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: RadialGradient(
-                    center: Alignment.center,
-                    radius: 1.0,
-                    colors: [
-                      Color.fromRGBO(44, 125, 247, 0.30), // 30%
-                      Color.fromRGBO(44, 125, 247, 0.20), // 20%
-                      Color.fromRGBO(44, 125, 247, 0.02), // 2%
-                    ],
-                    stops: [0.0, 0.53, 0.96],
-                  ),
-                ),
-              ),
+            top: -2.59,
+            left: 0,
+            child: Image.asset(
+              "assets/images/Ellipse.png", // apna ellipse image yahan rakho
+              width: 508.36,
+              height: 588.24,
+              fit: BoxFit.contain,
             ),
           ),
 
-          /// Center Text
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Language Translate",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.roboto(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+          /// Text Positioned with given properties
+          Positioned(
+            top: 158,
+            left: 20,
+            child: SizedBox(
+              width: 320,
+              height: 84,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Language Translate",
+                    style: GoogleFonts.roboto(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                Text(
-                  "App",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.roboto(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                  Text(
+                    "App",
+                    style: GoogleFonts.roboto(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
